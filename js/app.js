c23cf346ec9d2ebf41d195548ec545a082f5c565
Shap3shifter <69819693+Shap3shifter@users.noreply.github.com>
2020-12-08 06:14:50 -0800

    Updated app.js with buildTable and handleClick functions

--- js/app.js
+++ js/app.js
@@ -23,3 +23,25 @@ function buildTable(data) {
         );
     });
  
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered then filter using that date
+    if (date) {
+        //Apply 'filter' to the table data, keeping rows only where
+        //the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //Rebuild the table using the filtered data
+    //Without a date, filteredData will remain as original tableData.
+    buildTable(filteredData);
+};
+
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
